f22b36b5bd7da5c1612f0120408f1f8d4c690902
Michael Getachew Abebe <101546624+michaelgetachew-abebe@users.noreply.github.com>
2026-02-06 03:19:07 +0300

    Create local_executor_demo.py with Airflow DAG

---
+++ local_executor_demo.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    "local_executor_demo",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+    local_task = BashOperator(
+        task_id="local_task",
+        bash_command="echo 'local task running in scheduler pod'",
+    )
